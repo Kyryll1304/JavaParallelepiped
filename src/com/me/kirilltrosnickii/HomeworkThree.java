7dc7537b3113e40f66f627dcd4d4e45069a80ead
Kyryll1304 <kirilltrosnickii@me.com>
2023-01-21 19:16:56 +0200

    Primary commmit

--- src/com/me/kirilltrosnickii/HomeworkThree.java
+++ src/com/me/kirilltrosnickii/HomeworkThree.java
@@ -3,37 +3,47 @@ package com.me.kirilltrosnickii;
 import java.util.Scanner;
 
 public class HomeworkThree {
-    public static int number;
-    public static int numberOne;
-    public static int numberTwo;
+    public static int length;
+    public static int width;
+    public static int height;
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
         System.out.println("Введіть першу сторону");
-        Scanner scanOne = new Scanner(System.in);
-        System.out.println("Введіть другу сторону");
-        Scanner scanTwo = new Scanner(System.in);
-        System.out.println("Введіть третю сторону");
-
-        String line = scan.nextLine();
-        System.out.println("Перша сторона: " + line);
-        String lineOne = scanOne.nextLine();
-        System.out.println("Друга сторона: " + lineOne);
-        String lineTwo = scanTwo.nextLine();
-        System.out.println("Третя сторона: " + lineTwo);
-
-        try {
-            number = Integer.parseInt(line);
-            numberOne = Integer.parseInt(lineOne);
-            numberTwo = Integer.parseInt(lineTwo);
-        } catch (NumberFormatException ex) {
-            ex.printStackTrace();
+        if (scan.hasNextInt()) {
+            length = scan.nextInt();
+        } else {
+            System.out.println("Помилка,тільки цифри");
+            scan.close();
         }
 
-        int volume = number * numberOne * numberTwo;
-        int length = number + numberOne + numberTwo;
+        System.out.println("Введіть другу сторону");
+        if (scan.hasNextInt()) {
+            width = scan.nextInt();
+        } else {
+            System.out.println("Помилка,тільки цифри");
+            scan.close();
+        }
+
+        System.out.println("Введіть висоту");
+        if (scan.hasNextInt()) {
+            height = scan.nextInt();
+        } else {
+            System.out.println("Помилка,тільки цифри");
+            scan.close();
+        }
+
+
+        System.out.println("Перша сторона: " + length);
+        System.out.println("Друга сторона: " + width);
+        System.out.println("Висота: " + height);
+
+
+        int volume = length * width * height;
+        int summ = length + width + height;
         System.out.println("Об'єм паралелепіпеда дорівнює " + volume);
-        System.out.println("Сума заданих сторін паралелепіпеда дорівнює " + length + " ,а його периметр дорівнює " + length * 4);
+        System.out.println("Сума заданих сторін паралелепіпеда дорівнює " + summ + " ,а його периметр дорівнює " + summ * 4);
     }
 
 }
